bfea0461e388237ce626cfdaf21a6805579b7b34
Harry Gallagher <harrygallagher4@gmail.com>
2021-12-04 16:00:13 -0600

    nixpkgs-21.11-darwin

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
-    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-21.05-darwin;
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-21.11-darwin;
 
     darwin = {
       url = github:lnl7/nix-darwin/master;
@@ -11,7 +11,7 @@
     };
 
     home-manager = {
-      url = github:nix-community/home-manager;
+      url = github:nix-community/home-manager/release-21.11;
       # inputs.nixpkgs.follows = "nixpkgs";
     };
 
