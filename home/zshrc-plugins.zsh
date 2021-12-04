d220914aeee560f804320a375bc1edeb84815778
Harry Gallagher <harrygallagher4@gmail.com>
2021-12-04 15:55:52 -0600

    zdharma -> zdharma-continuum

--- home/zshrc-plugins.zsh
+++ home/zshrc-plugins.zsh
@@ -23,22 +23,22 @@ zstyle ":plugin:history-search-multi-word" clear-on-cancel "no"
 zinit wait lucid light-mode for \
     Aloxaf/fzf-tab \
   atinit'zicompinit; zicdreplay' \
-    zdharma/fast-syntax-highlighting \
+    zdharma-continuum/fast-syntax-highlighting \
   atload'_zsh_autosuggest_start; autosuggest-atload' \
     zsh-users/zsh-autosuggestions \
   blockf atpull'zinit creinstall -q .' \
     zsh-users/zsh-completions \
-  zdharma/history-search-multi-word
+  zdharma-continuum/history-search-multi-word
 
 zinit wait'1' lucid light-mode for \
-  zdharma/zui \
-  zdharma/zzcomplete
+  zdharma-continuum/zui \
+  zdharma-continuum/zzcomplete
 
 zinit ice depth=1
 zinit light romkatv/powerlevel10k
 
 zinit ice wait"2" lucid as"program" pick"bin/git-dsf"
-zinit light zdharma/zsh-diff-so-fancy
+zinit light zdharma-continuum/zsh-diff-so-fancy
 
 zinit ice as"program" src"init.sh"
 zinit light b4b4r07/enhancd
